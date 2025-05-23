1503f9063e3f9c23b095b9c4ad94290cd4f5967a
Mansh7777 <mansh@atroissoftware.com>
2025-05-23 17:24:37 +0530

    added

--- src/services/report.service.js
+++ src/services/report.service.js
@@ -4,28 +4,39 @@ const httpStatus = require('http-status');
 
 const mapReportData = (req, data, imageURIs, fileData) => {
   const userId = req.user._id;
+
   if (!imageURIs.length || imageURIs.length > 5) {
     throw new ApiError(httpStatus.BAD_REQUEST, 'You must upload between 1 to 5 images.');
   }
+
   if (!fileData || !fileData.uri) {
     throw new ApiError(httpStatus.BAD_REQUEST, 'Report file is required.');
   }
-  return {
+
+  const report = {
     userId: userId,
     companyName: data.body.companyName,
     address: data.body.address,
-    notes: data.body.notes,
     reportDate: data.body.reportDate,
     reportTime: data.body.reportTime,
-    businessSize: data.body.businessSize,
     file: {
       name: fileData.name,
       size: fileData.size,
       type: fileData.type,
-      url:  fileData.uri,
+      url: fileData.uri,
     },
     images: imageURIs.map((file) => file.imageURI),
   };
+
+  if (data.body.notes && data.body.notes.trim() !== '') {
+    report.notes = data.body.notes.trim();
+  }
+
+  if (data.body.businessSize && data.body.businessSize.trim() !== '') {
+    report.businessSize = data.body.businessSize.trim();
+  }
+
+  return report;
 };
 
 const createReport = async (req, data, imageURIs, fileURI) => {
@@ -33,6 +44,7 @@ const createReport = async (req, data, imageURIs, fileURI) => {
   const report = await Report.create(reportData);
   return report;
 };
+
 
 const getReportsByUser = async (userId) => {
   if (!userId) {
