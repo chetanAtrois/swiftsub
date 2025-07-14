addadbe3ffb3f74e447c4e83983a8e1a51aeabe9
Mansh7777 <mansh@atroissoftware.com>
2025-07-14 16:03:01 +0530

    added

--- src/app.js
+++ src/app.js
@@ -24,8 +24,8 @@ if (config.env !== 'test') {
 
 // Security & parsing middlewares
 app.use(helmet());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' })); // or higher if needed
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(xss());
 app.use(mongoSanitize());
 app.use(compression());
