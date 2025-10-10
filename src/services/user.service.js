6ddfe2642df24bf9ef9d59ebb4e06122aca0f18a
chetanece <vermachetan123456@gmail.com>
2025-10-10 12:17:02 +0530

    changees in user

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,5 +1,5 @@
 const httpStatus = require('http-status');
-const  User  = require('../models/admin.model');
+const  User  = require('../models/user.model');
 const ApiError = require('../utils/ApiError');
 const { responseMessage } = require('../constant/constant');
 const Admin = require("../models/admin.model");
